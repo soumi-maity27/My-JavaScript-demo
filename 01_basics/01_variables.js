0277799092a7a2fc88d0bdde9cabf498c5e0408a
soumi-maity27 <soumimaity230@gmail.com>
2025-08-27 06:41:42 +0000

    Added variable concepts

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=144553
+let accountEmail="maity@gmail.com"
+var accountPassword="12345"
+accountCity="Mumbai"
+
+accountEmail="maitysoumi@gmail.com"
+accountPassword="224567"
+accountCity="kolkata"
+let accountState
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
