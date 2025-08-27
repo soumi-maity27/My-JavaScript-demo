bab6c4faddd695c38a27416059b9d639a2ca37cb
soumi-maity27 <soumimaity230@gmail.com>
2025-08-27 07:00:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+//alert(3+3) we are using node.js not a browser
+
+let name="Soumi"
+let age=19
+let isLoggedIn=false
+let state;
+
+// number=>2 to power 53
+// bigint
+//string=> ""
+//boolean=> true/false
+//null=> standalone value
+//undefined =>
+// symbol=> unique
+
+//object
+
+console.log(typeof(isLoggedIn));
+console.log(typeof(state));
+
